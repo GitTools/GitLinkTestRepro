5c383a04e9fd854be2c4072ce74550dc6294c273
Geert van Horrik <geert@catenalogic.com>
2014-09-01 13:03:15 +0200

    Initial commit

---
+++ MyClassLibrary/MyClassLibrary/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyClassLibrary
+{
+    public class MyClass
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("My method");
+        }
+    }
+}
